9024ccf380ff34cbcabe04fa733e4146f0b738f6
Grace Maria <gracemaria321@gmail.com>
2025-10-19 23:49:45 -0500

    Cyber Crimes in India (NCRB)
    2003 - 2022

---
+++ IndianCyberThreats.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 19 23:27:26 2025
+
+@author: Grace Maria IIT
+"""
+
+import pandas as pd
+import plotly.express as px
+
+# Load dataset
+file_path = "DownloadedDatasets\CyberThreatsIndia\cyber-crimes-from-ncrb-master-data-year-state-and-city-wise-type-of-cyber-crimes-committed-in-violation-of-it-act.csv"
+df = pd.read_csv(file_path)
+
+# Group by year and offence_category, summing the values
+grouped = df.groupby(['year', 'offence_category'])['value'].sum().reset_index()
+
+# Create a stacked bar chart
+fig = px.bar(
+    grouped,
+    x='year',
+    y='value',
+    color='offence_category',
+    title='Cyber Crimes in India (NCRB): Offence Categories Over Years',
+
+    labels={'year': 'Year', 'value': 'Number of Offences', 'offence_category': 'Offence Category'},
+    width=1536, height=1024
+)
+
+# Save the plot
+fig.write_image("india_cyber_crimes.png")
+fig.write_json("india_cyber_crimes.json")
